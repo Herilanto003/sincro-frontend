acd6a9706caa048b9ecf529a48f60571f6280e4c
unknown <herilantodenis@gmail.com>
2024-06-08 10:16:40 +0300

    starting of the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className='text-4xl text-center my-4'>
+            application sincro
+        </div>
+    )
+}
+
+export default App

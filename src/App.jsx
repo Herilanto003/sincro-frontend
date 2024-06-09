eadc79b17c3618f0a0443a31ec8a86993cdb8907
unknown <herilantodenis@gmail.com>
2024-06-09 17:02:48 +0300

    add some packages...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-    return (
-        <div className='text-4xl text-center my-4'>
-            application sincro
-        </div>
-    )
+  return (
+    <div className="text-4xl text-center my-4 text-secondary-500 font-bold bg-primary-50">
+      application sincro
+    </div>
+  );
 }
 
-export default App
+export default App;
